28dd9c451d91a4949c4b65f688401289275c5f04
Kishore K <zishwrld@gmail.com>
2026-06-11 16:14:32 +0530

    chore: enforce www canonical URLs

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://quantalabs.cc/sitemap.xml',
+    sitemap: 'https://www.quantalabs.cc/sitemap.xml',
   }
 }
