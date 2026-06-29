c541fa57360c7d2e9bdbc548798151521b5e3626
Kishore K <zishwrld@gmail.com>
2026-06-29 18:58:46 +0530

    seo

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://www.quantalabs.cc',
   generateRobotsTxt: true, // Generate robots.txt file
+  generateIndexSitemap: false,
   sitemapSize: 7000,
   robotsTxtOptions: {
     policies: [
